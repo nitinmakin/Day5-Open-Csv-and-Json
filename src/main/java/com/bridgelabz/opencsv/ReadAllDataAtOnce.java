cea4f124eaee4cc76c00035b0c0b7820a6341306
nitinmakin <nitinmakin124.com>
2020-09-03 00:24:31 +0530

    Added-Read all data at once

---
+++ src/main/java/com/bridgelabz/opencsv/ReadAllDataAtOnce.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.opencsv;
+import com.opencsv.CSVReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+public class ReadAllDataAtOnce {
+    public static void main(String[] args)  {
+        try {
+            FileReader filereader = new FileReader("./users.csv");
+            CSVReader csvReader = new CSVReader(filereader);
+
+            // Reading all  Records at Once
+            List<String[]> allData = csvReader.readAll();
+
+            for (String[] record : allData) {
+                System.out.println("Name : " + record[0]);
+                System.out.println("Email : " + record[1]);
+                System.out.println("Phone : " + record[2]);
+                System.out.println("Country : " + record[3]);
+                System.out.println("==========================");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
